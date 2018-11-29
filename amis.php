a3c18ba9f6185c82f171ddb18d33c0a2cb3fd0b6
vHyPo <44779553+vHyPo@users.noreply.github.com>
2018-11-29 16:59:03 +0100

    Ajouts
    Membre.php
    Amis.php
    (  ajout d'amis, recherche par NOM DE FAMILLE, affichage d'amis et des membres  )

---
+++ amis.php
@@ -0,0 +1,63 @@
+<?php
+//Ajout du head de page
+include('tools/head.inc.php');
+$myId = $GLOBAL_ouser->get_idUser();
+$SQL_amis = "SELECT * FROM user,amis WHERE amis.idUser2 = user.idUser AND idUser1 = $myId";
+$req = $conn->query($SQL_amis);
+?>
+
+  <div id="content-wrapper">
+
+    <div class="container-fluid">
+      <h3>Mes amis</h3>
+      <div class="col-lg">
+        <div class="row">
+          <div class="col-md-24">
+            <div class="row">
+              <div class="col-md-8">
+                <div class="row">
+              <?php
+                while($res = $req ->fetch())
+                {
+                ?>
+                <div class="col-md-8">
+                  <div class="row">
+                    <div class="col-md-4">
+                      <img src="image/<?php echo $res['photoUser']; ?>" class="img-fluid img-thumbnail rounded" alt="">
+                    </div>
+                    <div class="col-md-6">
+                      <p> <a href="profile.php?user=<?php echo $res['idUser']; ?>"><?php echo $res["nameUser"]." ".$res["preUser"]; ?></a> </p>
+                      <p> <?php echo $res["mailUser"]; ?> </p>
+
+                      <p><?php echo $res["descUser"]; ?></p>
+                    </div>
+                  </div>
+                </div>
+              <?php
+                }
+              ?>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- /.container-fluid -->
+
+    <!-- Sticky Footer -->
+    <footer class="sticky-footer">
+      <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+          <span>Copyright © Your Website 2018</span>
+        </div>
+      </div>
+    </footer>
+
+  </div>
+  <!-- /.content-wrapper -->
+
+<?php
+//ajout du pied de page
+include('tools/foot.inc.php');
+ ?>
--- profileOther.php
+++ profileOther.php
@@ -1,13 +1,17 @@
 <?php
 //Ajout du head de page
 include('tools/head.inc.php');
-if(isset($_GET['user']))
+$idUser = $_GET['user'];
+
+$SQL_user = "SELECT idStage,datedebStage,datefinStage,libStage,descStage,nameEntreprise,u.idUser,nameUser,preUser,mailUser,loginUser,descUser,photoUser
+              FROM user u
+              LEFT JOIN stage s ON u.idUser = s.idUser
+              LEFT JOIN entreprise e ON s.idEntreprise = e.idEntreprise
+              WHERE u.idUser = '$idUser'";
+
+$req = $conn->query($SQL_user);
+while ($res = $req -> fetch())
 {
-  if($GLOBAL_ouser->get_idUser() != $_GET['user'])
-  {
-    echo "<script type='text/javascript'>document.location.replace('profileOther.php?user=$_GET[user]');</script>";
-  }
-}
 ?>
 
   <div id="content-wrapper">
@@ -20,27 +24,27 @@ if(isset($_GET['user']))
             <div class="row">
 
               <div class="col-md-6">
-                <img class="pp" src="image/<?php echo $GLOBAL_ouser->get_photoUser(); ?>" class="img-fluid" alt="Photo de profil" style="border-radius:50%;width:120px;height:120px;margin-top:15px;margin-left:20px;"> <!-- IMAGE -->
+                <img class="pp" src="image/<?php echo $res['photoUser']; ?>" class="img-fluid" alt="Photo de profil" style="border-radius:50%;width:120px;height:120px;margin-top:15px;margin-left:20px;"> <!-- IMAGE -->
               </div>
               <div class="col-lg-3 col-md-3 col-sm-5 col-xs-5" style="margin-top:2%;">
                 <div class="col-md"> <!-- NOM -->
-                  <h4 class="h4"><?php echo $GLOBAL_ouser->get_nameUser(); ?></h4>
+                  <h4 class="h4"><?php echo $res['nameUser']; ?></h4>
                 </div>
                 <div class="col-md"> <!-- PRENOM -->
-                  <h5 class="h5"><?php echo $GLOBAL_ouser->get_preUser(); ?></h5>
+                  <h5 class="h5"><?php echo $res['preUser']; ?></h5>
                 </div>
               </div>
             </div>
           </div>
           <div class="col-md col-lg col-ms col-xs" style="color:#007BFF;"> <!-- Boutons d'info -->
             <div class="col-md-5 col-sm-3">
-              <a href="#" style="text-decoration:none;"><i class="fas fa-user-friends"></i> Liste d'amis</a>
+
             </div>
             <div class="col-md-5 col-sm-3">
-              <a href="#" style="text-decoration:none;"><i class="fas fa-address-book"></i> Mes coordonnées</a>
+
             </div>
             <div class="col-md-5 col-sm-3">
-              <a href="pref.php"  style="text-decoration:none;"><i class="fas fa-edit"></i>Modifier mes informations</a>
+
             </div>
 
           </div>
@@ -52,8 +56,7 @@ if(isset($_GET['user']))
               <br>
                   <hr class="separator"> <!-- SEPARATEUR -->
               <br>
-              <a href="#" onclick="changeState()" style="text-decoration:none;"><i class="fas fa-edit"></i></a>
-              <textarea id='textarea' onblur="register()" readonly value="textProfile" rows="8" cols="80" style="resize:none;box-shadow:2px 2px 10px #888888;outline:0;"><?php echo $GLOBAL_ouser->get_descUser();?></textarea>
+              <textarea id='textarea' onblur="register()" readonly value="textProfile" rows="8" cols="80" style="resize:none;box-shadow:2px 2px 10px #888888;outline:0;"><?php echo $res['descUser'];?></textarea>
             </div>
           </div>
         </div>
@@ -72,28 +75,18 @@ if(isset($_GET['user']))
           </div>
         </div>
       </section>
+      <?php
+        if($res['idStage'] != NULL)
+        {
+      ?>
       <section class="stage" style="box-shadow:2px 5px 18px #888888;margin-top:1.5%;border-radius:3px;border:1px solid rgba(0,0,0,0.15)">
         <div class="col-lg-14">
           <div class='row' style='margin:5px;'>
             <div class="col-md-10">
               <p class="h4">Stages</p>
             </div>
-            <div class='col-sm-1' style='margin-right:2px;'>
-              <button class='btn btn-secondary btn-sm' type='button' onclick="document.location.replace('stagecrea.php');"><i class="fas fa-plus"></i></button>
-            </div>
-          </div>
-          <!-- début affichage stage BDD -->
-          <?php
-          $idUser = $GLOBAL_ouser->get_idUser();
-            $sql = "SELECT datedebStage,datefinStage,libStage,descStage,nameEntreprise
-                    FROM stage s,entreprise e
-                    WHERE s.idEntreprise = e.idEntreprise
-                    AND s.idUser = '$idUser'";
 
-            $req = $conn -> query($sql)or die($sql);
-            while ($res = $req -> fetch())
-            {
-              ?>
+          </div>
               <div class="col-md-10" style="box-shadow:2px 5px 18px #888888;margin:2%;"> <!-- CONTENU DU/DES STAGE(S)-->
                 <div class='row' >
                   <div class="col-md-4">
@@ -114,6 +107,7 @@ if(isset($_GET['user']))
                 <p><?php echo $res['descStage'];?> </p>
               </div>
               <?php
+              }
             }
           ?>
           <!-- fin affichage BDD -->
